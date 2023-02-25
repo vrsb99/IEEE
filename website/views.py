e17ac4cb7d0f956d3e4f16bf78dbad88490ab50e
vrsb99 <vigneshrsb99@gmail.com>
2023-02-25 19:33:54 +0800

    menu

--- website/views.py
+++ website/views.py
@@ -11,6 +11,7 @@ def homepage():
     categories = Categories.query.filter_by(user_id=current_user.id).all()
     store_name = store.name if store else None
     store_location = store.location if store else None
+    store_id = store.id if store else None
     
     if request.method == 'POST':
         data = request.form
@@ -35,20 +36,20 @@ def homepage():
     
             
     if store_name and store_location and categories:
-        return render_template("homepage.html", user=current_user, store_name=store_name, store_location=store_location, categories=categories)
+        return render_template("homepage.html", user=current_user, store_name=store_name, store_location=store_location, categories=categories, store_id=store_id)
     elif store_name and store_location:
-        return render_template("homepage.html", user=current_user, store_name=store_name, store_location=store_location)
+        return render_template("homepage.html", user=current_user, store_name=store_name, store_location=store_location, store_id=store_id)
     else:
         return render_template("homepage.html", user=current_user)
 
-@views.route('/add_category', methods=['GET', 'POST'])
+@views.route('/add_category/<int:store_id>', methods=['GET', 'POST'])
 @login_required
-def add_categories():
+def add_categories(store_id):
     if request.method == 'POST':
         data = request.form
         add_category = data.get('add_category')
         if add_category:
-            new_category = Categories(name=add_category, user_id=current_user.id)
+            new_category = Categories(name=add_category, user_id=current_user.id, store_id=store_id)
             db.session.add(new_category)
             db.session.commit()
             flash('Category added successfully', category='success')
@@ -57,16 +58,16 @@ def add_categories():
     return render_template("add_category.html", user=current_user)
 
 
-@views.route('/category/<int:category_id>', methods=['GET', 'POST'])
-def items(category_id):
-    category = Categories.query.filter_by(id=category_id).first() 
+@views.route('/category/<int:store_id>/<int:category_id>', methods=['GET', 'POST'])
+def items(store_id, category_id):
+    category = Categories.query.filter_by(id=category_id).first()
     items = Products.query.filter_by(category_id=category_id).all()
     
-    return render_template("items.html", user=current_user, category=category, items=items)
+    return render_template("items.html", user=current_user, category=category, items=items, store_id=store_id)
 
-@views.route('/add_item/<int:category_id>', methods=['GET', 'POST'])
+@views.route('/add_item/<int:store_id>/<int:category_id>', methods=['GET', 'POST'])
 @login_required
-def add_items(category_id):
+def add_items(store_id, category_id):
     if request.method == 'POST':
         data = request.form
         item_name = data.get('item_name')
@@ -74,11 +75,11 @@ def add_items(category_id):
         item_price = data.get('item_price')
         
         if item_name and item_price and item_description:
-            new_item = Products(name=item_name, description=item_description, price=item_price, user_id=current_user.id, category_id=category_id)
+            new_item = Products(name=item_name, description=item_description, price=item_price, user_id=current_user.id, category_id=category_id, store_id=store_id)
             db.session.add(new_item)
             db.session.commit()
             flash('Item added successfully', category='success')
-            return redirect(url_for('views.items', category_id=category_id))
+            return redirect(url_for('views.items', category_id=category_id, store_id=store_id))
     
     
     return render_template("add_item.html", user=current_user)
