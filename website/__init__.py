41fb6a84bf867ba46a732e4b481f9ef0024aa553
vrsb99 <vigneshrsb99@gmail.com>
2023-02-25 11:00:31 +0800

    Setup

---
+++ website/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "clients.db"
+
+def config_app():
+    app = Flask(__name__)
+    app.static_folder = 'static'
+    app.config['SECRET_KEY'] = 'IEEE'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
+    
+    # Setup for database
+    with app.app_context():
+        db.create_all()
+    
+    return app
