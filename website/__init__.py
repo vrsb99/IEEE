63a88fadad123fb3a9603fa1cef2fd4faf4d814c
vrsb99 <vigneshrsb99@gmail.com>
2023-02-26 10:31:34 +0800

    Revert "refactor"
    This reverts commit 7ee1c1257360337a8ce89b6bc845093597e9141e.

--- website/__init__.py
+++ website/__init__.py
@@ -1,48 +1,48 @@
 from flask import Flask, request, send_file
 from flask_sqlalchemy import SQLAlchemy
-import os
+from os import path
 from flask_login import LoginManager
 from flask_qrcode import QRcode
 
 db = SQLAlchemy()
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///clients.db')
+DB_NAME = "clients.db"
 
 def config_app():
     app = Flask(__name__)
     app.static_folder = 'static'
     app.config['SECRET_KEY'] = 'IEEE'
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     qr_code = QRcode(app)
-
+    
     # Importing the blueprints
     from .views import views
     from .auth import auth
     from .menu import menu
-
+    
     # Registering the blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth')
     app.register_blueprint(menu, url_prefix='/menu')
-
-    from .models import User
-
+    
+    from .models import User, Stores, Categories, Products
+    
     # Setup for login
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
+    
     @app.route("/qrcode", methods=["GET"])
     def get_qrcode():
         # please get /qrcode?data=<qrcode_data>
         data = request.args.get("data", "")
         return send_file(qr_code(data, mode="raw"), mimetype="image/png")
-
+    
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
-
-
+    
+    
     # Setup for database
     with app.app_context():
         try:
@@ -50,5 +50,5 @@ def config_app():
         except Exception:
             db.drop_all()
             db.create_all()
-
+            
     return app
